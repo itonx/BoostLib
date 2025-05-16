488566d70a10c2073a913925ea2e469721c1a84b
Ayrton Teniente <ayrton.94.e@gmail.com>
2025-05-15 23:17:19 -0600

    Add project files.

---
+++ Env.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+
+namespace BoostLib
+{
+    public static class Env
+    {
+        public static IEnumerable<string> GetVariableNames()
+        {
+            return Environment.GetEnvironmentVariables().Keys.Cast<string>();
+        }
+
+        public static Dictionary<string, string?> GetVariables()
+        {
+            return Environment.GetEnvironmentVariables().Cast<DictionaryEntry>().ToDictionary(kv => (string)kv.Key, kv => (string?)kv.Value);
+        }
+
+        public static string? GetVariableValue(string variableName)
+        {
+            return Environment.GetEnvironmentVariable(variableName);
+        }
+    }
+}
